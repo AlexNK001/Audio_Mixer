8d060c731e2d1b3245d2f56586b7a1c79498c0cf
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2024-10-17 10:57:52 +0300

    init

---
+++ Assets/SoundSlider.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundSlider : MonoBehaviour
+{
+    [SerializeField] protected AudioMixerGroup _mixer;
+    [SerializeField] protected Slider _slider;
+
+    protected readonly float _maxSoundValue = 1f;
+    protected readonly float _minSoundValue = 0.0001f;
+    protected string _parametrName;
+    protected float _correctionNumber = 20f;
+
+    public float Volume { get; protected set; }
+
+    public void Initilization(string parametrName, float volume)
+    {
+        _parametrName = parametrName;
+        Volume = volume;
+        _slider.value = Volume;
+    }
+
+    public void ChangeVolume(float value)
+    {
+        value = Mathf.Clamp(value, _minSoundValue, _maxSoundValue);
+        Volume = value;
+        _mixer.audioMixer.SetFloat(_parametrName, Mathf.Log10(Volume) * _correctionNumber);
+    }
+}
